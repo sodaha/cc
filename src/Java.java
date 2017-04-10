f5434af12dabe10ff22482a265f3d2287febbf51
unknown <tomnoii.da@gmail.com>
2017-04-10 13:10:15 +0700

    ddddd

--- src/Java.java
+++ src/Java.java
@@ -1,17 +1,14 @@
 
 public class Java {
-	void ABC (int x){
-		int a=2;
-		a=a+x;
+	void ABC(int x) {
+		int a = 2;
+		a = a + x;
 		System.out.println(a);
 	}
 
 	public static void main(String[] args) {
 		Java x = new Java();
 		x.ABC(8);
-		
-
-
 
 	}
 
