73502464b5df5f7c94d807b5acc523bf7f8a1867
unknown <tomnoii.da@gmail.com>
2017-04-10 13:05:19 +0700

    abc

---
+++ src/Java.java
@@ -0,0 +1,18 @@
+
+public class Java {
+	void ABC (int x){
+		int a=2;
+		a=a+x;
+		System.out.println(a);
+	}
+
+	public static void main(String[] args) {
+		Java x = new Java();
+		x.ABC(8);
+		
+
+
+
+	}
+
+}
